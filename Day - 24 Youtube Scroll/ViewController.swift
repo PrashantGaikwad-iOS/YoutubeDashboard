b19d93eda963d4031f3b7fee1a52748dcd935036
Prashant Gaikwad <prashant.gaikwad@leftrightmind.com>
2018-02-20 12:17:58 +0530

    Initial Commit

---
+++ Day - 24 Youtube Scroll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Day - 24 Youtube Scroll
+//
+//  Created by Prashant Gaikwad on 2/20/18.
+//  Copyright © 2018 Prashant Gaikwad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
